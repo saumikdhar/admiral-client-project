bbffe6ff20189a90d8f8c53d63ee6a50e3a35e00
Saumik Dhar <dhars@cardiff.ac.uk>
2019-11-15 10:09:15 +0000

    covert tables to plural in schema and resolve the plural issue in data

--- Schema.sql
+++ Schema.sql
@@ -1,60 +1,61 @@
-SHOW DATABASES;
+ SHOW DATABASES;
 
+DROP SCHEMA IF EXISTS ManagingTimeSheet;
 CREATE SCHEMA IF NOT EXISTS ManagingTimeSheet;
 
 USE ManagingTimeSheet;
 
-CREATE TABLE IF NOT EXISTS Login(
+CREATE TABLE IF NOT EXISTS Logins(
 login_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(150) NOT NULL,
 password VARCHAR(12) NOT NULL,
 access_level int(1) NOT NULL)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS Agency(
+CREATE TABLE IF NOT EXISTS Agencies(
 agency_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 agency_name VARCHAR(100) NOT NULL)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS Manager(
+CREATE TABLE IF NOT EXISTS Managers(
 manager_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 manager_firstName VARCHAR(100) NOT NULL,
 manager_lastName VARCHAR(100) NOT NULL,
 login_id int,
- FOREIGN KEY (login_id) REFERENCES Login(login_id) 
+ FOREIGN KEY (login_id) REFERENCES Logins(login_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE    
  )
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS Contractor(
+CREATE TABLE IF NOT EXISTS Contractors(
 contractor_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 contractor_firstName VARCHAR(100) NOT NULL,
 contractor_lastName VARCHAR(100) NOT NULL,
 login_id int,
 manager_id int,
- FOREIGN KEY (login_id) REFERENCES Login(login_id) 
+ FOREIGN KEY (login_id) REFERENCES Logins(login_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE,
- FOREIGN KEY (manager_id) REFERENCES Manager(manager_id) 
+ FOREIGN KEY (manager_id) REFERENCES Managers(manager_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE
  )
 ENGINE = InnoDB;
 
 
-CREATE TABLE IF NOT EXISTS Admin(
+CREATE TABLE IF NOT EXISTS Admins(
 admin_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 admin_firstName VARCHAR(100) NOT NULL,
 admin_lastName VARCHAR(100) NOT NULL,
 login_id int,
- FOREIGN KEY (login_id) REFERENCES Login(login_id)
+ FOREIGN KEY (login_id) REFERENCES Logins(login_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE
  )
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS TimeSheet(
+CREATE TABLE IF NOT EXISTS TimeSheets(
 timesheet_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 days_worked VARCHAR(100) NOT NULL,
 overtime int NOT NULL,
@@ -63,18 +64,18 @@ status VARCHAR(10)
  )
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS Agency_Contractor(
+CREATE TABLE IF NOT EXISTS Agency_Contractors(
 agency_contractor_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 agency_id int,
 contractor_id int,
 timesheet_id int,
- FOREIGN KEY (agency_id) REFERENCES Agency(agency_id)
+ FOREIGN KEY (agency_id) REFERENCES Agencies(agency_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE,
- FOREIGN KEY (contractor_id) REFERENCES Contractor(contractor_id)
+ FOREIGN KEY (contractor_id) REFERENCES Contractors(contractor_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE,
- FOREIGN KEY (timesheet_id) REFERENCES TimeSheet(timesheet_id)
+ FOREIGN KEY (timesheet_id) REFERENCES TimeSheets(timesheet_id)
  ON UPDATE CASCADE
  ON DELETE CASCADE
  )
