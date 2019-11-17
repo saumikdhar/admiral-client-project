13020e44a1808ee0ac7ba13ba66e13422071aa8f
Gabriel Agius <AgiusG@cardiff.ac.uk>
2019-11-17 21:15:38 +0000

    changed button link

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ContractorController.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ContractorController.java
@@ -1,13 +1,14 @@
 package com.nsa.team9.timesheetmanager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ContractorController {
 
-@RequestMapping(path = "/TimeSheetConfirmation", method = RequestMethod.POST)
+@RequestMapping(path = "/TimeSheetConfirmation", method = RequestMethod.GET)
 public String TimeSheetConfirmation(){
 
     return "timesheet_confirmation";
