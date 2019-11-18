7afaf42b46a2bcffe8812ad89b1c7d2b549d8f5d
Gabriel Agius <AgiusG@cardiff.ac.uk>
2019-11-18 15:56:57 +0000

    added all necessary validation required for form

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/TimeSheetForm.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/TimeSheetForm.java
@@ -1,5 +1,38 @@
 package com.nsa.team9.timesheetmanager.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TimeSheetForm {
-    
+
+    @NotNull
+    private Date start_date;
+
+
+    private Boolean monday_worked;
+
+
+    private Boolean tuesday_worked;
+
+
+    private Boolean wednesday_worked;
+
+
+    private Boolean thursday_worked;
+
+
+    private Boolean friday_worked;
+
+    @NotNull
+    @Size(min = 0, max = 20, message = "Invalid overtime hours")
+    private int overtime;
+
 }
