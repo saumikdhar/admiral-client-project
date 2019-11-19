cd6f7b3f85307ed2c57f03d84d04baba5f0aa818
Gabriel Agius <AgiusG@cardiff.ac.uk>
2019-11-19 17:55:07 +0000

    added an application class for tests

--- time-sheet-manager/src/test/java/ContractorFormTest.java
+++ time-sheet-manager/src/test/java/ContractorFormTest.java
@@ -1,2 +1,53 @@
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = Application.class)
 public class ContractorFormTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void CheckIfTimeSheetDetailsAreOk() throws Exception{
+
+        mvc.perform
+                (get
+                        ("TimeSheetDetails")
+                )
+                .andDo(
+                        print()
+                )
+                .andExpect(
+                        status().isOk()
+                )
+                .andExpect(
+                        content().string(containsString("Thank you for submitting your time sheet"))
+                )
+
+
+        ;
+
+
+    }
+
+
 }
