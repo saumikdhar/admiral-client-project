cebd4192655b0344fb705137b4e34413b76efb1a
Gabriel Agius <AgiusG@cardiff.ac.uk>
2019-11-19 18:47:54 +0000

    Added a test that only half works

--- time-sheet-manager/src/test/java/ContractorFormTest.java
+++ time-sheet-manager/src/test/java/ContractorFormTest.java
@@ -1,4 +1,6 @@
 
+import com.nsa.team9.timesheetmanager.controllers.TimeSheetForm;
+import com.nsa.team9.timesheetmanager.domain.TimeSheet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,11 @@ import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,11 +34,17 @@ public class ContractorFormTest {
     private MockMvc mvc;
 
     @Test
-    public void CheckIfTimeSheetDetailsAreOk() throws Exception{
+    public void makeTimeSheetAndCheckIfFormSubmits() throws Exception {
+
+        String sDate1="13/11/2019";
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+
+        TimeSheetForm TestTimeSheetForm = new TimeSheetForm(date1,Boolean.TRUE,Boolean.FALSE,Boolean.FALSE,Boolean.FALSE,Boolean.FALSE,2);
 
         mvc.perform
                 (get
-                        ("TimeSheetDetails")
+                        ("/TimeSheetDetails") //final click in journey is a get.  everything is on the session at this point
+                        .sessionAttr("TimeSheetForm", TestTimeSheetForm)
                 )
                 .andDo(
                         print()
@@ -40,14 +53,12 @@ public class ContractorFormTest {
                         status().isOk()
                 )
                 .andExpect(
-                        content().string(containsString("Thank you for submitting your time sheet"))
+                        content().string(containsString("Thank you for submitting your time sheet")) //check for text from receipt page
                 )
-
-
         ;
-
-
     }
+
+
 
 
 }
