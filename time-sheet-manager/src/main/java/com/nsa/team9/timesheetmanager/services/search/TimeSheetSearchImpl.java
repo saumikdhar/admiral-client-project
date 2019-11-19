521e7e6d4fb1bcc9bc99ea9fb5aedf01237b6ac8
Saumik Dhar <dhars@cardiff.ac.uk>
2019-11-19 12:18:56 +0000

    modify css

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/services/search/TimeSheetSearchImpl.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/services/search/TimeSheetSearchImpl.java
@@ -3,7 +3,7 @@ package com.nsa.team9.timesheetmanager.services.search;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TimeSheetSearchStatic implements TimeSheetSearch {
+public class TimeSheetSearchImpl implements TimeSheetSearch {
 
 //    private TimeSheetRepository timeSheetRepository;
 }
