47587c905aab45f5572637310a2ba6e8825bd597
c1825201 <rogerscs2@cardiff.ac.uk>
2019-11-20 19:56:19 +0000

    validation added to notes window

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ManagerController.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ManagerController.java
@@ -2,6 +2,8 @@ package com.nsa.team9.timesheetmanager.controllers;
 
 
 import com.nsa.team9.timesheetmanager.controllers.util.ManagerNotes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,21 +17,35 @@ import java.util.List;
 @SessionAttributes({"note"})
 public class ManagerController {
 
+    static final Logger LOG = LoggerFactory.getLogger(ManagerController.class);
+
     @GetMapping("/manager")
     public String showDashboard(Model model) {
+        //remove once jpa added
         List<String> tests = new ArrayList<>();
         tests.add("Test1");
         tests.add("Test2");
         model.addAttribute("tests",tests);
+        //
         model.addAttribute("note", new ManagerNotes());
         return "manager";
     }
 
     @RequestMapping(value = "manager/addNotes", method = RequestMethod.POST)
     public String saveRejectedNotes(@ModelAttribute("note") @Valid ManagerNotes note,
-                                    Model model,
-                                    BindingResult bindingResult) {
-
+                                    BindingResult bindingResult,
+                                    Model model) {
+        if (bindingResult.hasErrors()) {
+            LOG.error(bindingResult.toString());
+            LOG.error("Manager addNotes has errors");
+            //remove once jpa added
+            List<String> tests = new ArrayList<>();
+            tests.add("Test1");
+            tests.add("Test2");
+            model.addAttribute("tests",tests);
+            //
+            return "redirect:/manager";
+        }
         return "redirect:/manager";
     }
 
--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/util/ManagerNotes.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/util/ManagerNotes.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ManagerNotes {
 
+    @NotNull
+
+    @Size(min=2, max=200, message = "Invalid Number of Characters")
     private String notes;
 }
