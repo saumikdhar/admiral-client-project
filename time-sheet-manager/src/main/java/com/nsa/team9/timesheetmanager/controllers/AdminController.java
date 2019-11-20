567fa1eee4ba69bec090ee0560336603ff2db619
Saumik Dhar <dhars@cardiff.ac.uk>
2019-11-20 12:50:18 +0000

    implement tests

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/AdminController.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/AdminController.java
@@ -1,6 +1,5 @@
 package com.nsa.team9.timesheetmanager.controllers;
 
-import jdk.internal.jline.internal.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,15 +12,8 @@ import java.util.ArrayList;
 public class AdminController {
 
 
-@GetMapping("admin")
+@GetMapping("/admin")
 public String showtimesheets(Model model){
-    String[] james = {"Co","Gavin Meredith","worked", "worked", "not worked", "not worked", "worked", "approved", "Ben Davies"};
-    ArrayList<String[]> timesheets = new ArrayList<>();
-    timesheets.add(james);
-    model.addAttribute("timesheets", timesheets);
-    String[] timesheet = timesheets.get(0);
-    ;
-//    Log.debug(timesheet[0]);
     return "adminshowtimesheets";
 }
 }
