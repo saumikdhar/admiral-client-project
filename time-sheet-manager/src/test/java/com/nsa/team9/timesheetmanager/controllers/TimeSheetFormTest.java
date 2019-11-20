852e3e428dc1572ddadced88b5b4ab4db4c381c1
Saumik Dhar <dhars@cardiff.ac.uk>
2019-11-20 00:37:50 +0000

    update test

--- time-sheet-manager/src/test/java/com/nsa/team9/timesheetmanager/controllers/TimeSheetFormTest.java
+++ time-sheet-manager/src/test/java/com/nsa/team9/timesheetmanager/controllers/TimeSheetFormTest.java
@@ -55,7 +55,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
                             .param("friday_worked", "false")
                             .param("overtime", "2")
                     )
-                    .andDo(
+                    .andDo( 
                             print()
                     )
                     .andExpect(
