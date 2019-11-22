5b394fcb6df0e17f95ef7133179c87d335c422d3
c1825201 <rogerscs2@cardiff.ac.uk>
2019-11-22 12:59:38 +0000

    added domain for agency contractor and editied timesheet domain to join properly

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/TimeSheetRepositoryJpa.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/TimeSheetRepositoryJpa.java
@@ -11,10 +11,12 @@ import java.util.Optional;
 public interface TimeSheetRepositoryJpa extends JpaRepository<TimeSheet, Long>, TimeSheetRespository {
     /*Queries go in here*/
 
-    @Query(value = "select * from timesheets join " +
-            "agency_contractors ac on timesheets.agency_contractor_id = " +
-            "ac.agency_contractor_id join contractors c2 on ac.contractor_id = " +
-            "c2.contractor_id join managers m on c2.manager_id = m.manager_id where " +
-            "m.manager_firstName = :firstName and m.manager_lastName = :lastName and status = 'pending'", nativeQuery = true)
-    public List<TimeSheet> getTimeSheetsByManager(@Param("lastName") String lastName,@Param("firstName") String firstName);
+//  Gets all time sheets where status is pending and for the relevant manager
+
+//    @Query(value = "select * from timesheets join " +
+//            "agency_contractors ac on timesheets.agency_contractor_id = " +
+//            "ac.agency_contractor_id join contractors c2 on ac.contractor_id = " +
+//            "c2.contractor_id join managers m on c2.manager_id = m.manager_id where " +
+//            "m.manager_firstName = :firstName and m.manager_lastName = :lastName and status = 'pending'", nativeQuery = true)
+//    public List<TimeSheet> getTimeSheetsByManager(@Param("lastName") String lastName,@Param("firstName") String firstName);
 }
