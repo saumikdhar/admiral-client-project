28a707caf758f97c9eb5002bb01fd74c9aa78775
c1825201 <rogerscs2@cardiff.ac.uk>
2019-11-22 12:46:59 +0000

    started adding sql to timesheet jpa to get timesheets for manager dashboard

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/TimeSheetRepositoryJpa.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/TimeSheetRepositoryJpa.java
@@ -2,7 +2,19 @@ package com.nsa.team9.timesheetmanager.repositories;
 
 import com.nsa.team9.timesheetmanager.domain.TimeSheet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TimeSheetRepositoryJpa extends JpaRepository<TimeSheet, Long>, TimeSheetRespository {
     /*Queries go in here*/
+
+    @Query(value = "select * from timesheets join " +
+            "agency_contractors ac on timesheets.agency_contractor_id = " +
+            "ac.agency_contractor_id join contractors c2 on ac.contractor_id = " +
+            "c2.contractor_id join managers m on c2.manager_id = m.manager_id where " +
+            "m.manager_firstName = :firstName and m.manager_lastName = :lastName and status = 'pending'", nativeQuery = true)
+    public List<TimeSheet> getTimeSheetsByManager(@Param("lastName") String lastName,@Param("firstName") String firstName);
 }
