a2fdd2663e4b2b1ca14547a290adf06fcd2b5656
Saumik Dhar <dhars@cardiff.ac.uk>
2019-11-24 14:15:22 +0000

    resolve error with hiberntae-agency

--- time-sheet-manager/src/test/java/com/nsa/team9/timesheetmanager/domain/AgencyTest.java
+++ time-sheet-manager/src/test/java/com/nsa/team9/timesheetmanager/domain/AgencyTest.java
@@ -1,26 +1,15 @@
 package com.nsa.team9.timesheetmanager.domain;
 
-import com.nsa.team9.timesheetmanager.controllers.util.AgencyJpaConfig;
 import com.nsa.team9.timesheetmanager.repositories.AgencyRepositry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
-import javax.persistence.EntityManager;
-
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
