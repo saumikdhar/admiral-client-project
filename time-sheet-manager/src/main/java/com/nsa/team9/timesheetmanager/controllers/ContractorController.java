1b9c5631b845cb85cd5d4b4f76bc2feea46c790f
Gabriel Agius <AgiusG@cardiff.ac.uk>
2019-11-25 12:09:45 +0000

    i have added a method to save the timesheet data into the database

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ContractorController.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ContractorController.java
@@ -1,15 +1,31 @@
 package com.nsa.team9.timesheetmanager.controllers;
 
+import com.nsa.team9.timesheetmanager.domain.TimeSheet;
+import com.nsa.team9.timesheetmanager.repositories.TimeSheetRepositoryJpa;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.sql.Time;
+import java.time.LocalDate;
 
+@Service
 @Controller
 public class ContractorController {
+
+    private TimeSheetRepositoryJpa TimeSheetCreator ;
+
+    public ContractorController(TimeSheetRepositoryJpa aCreator){
+        TimeSheetCreator = aCreator;
+    }
+
+    static final Logger LOG = LoggerFactory.getLogger(ContractorController.class);
 
 @GetMapping("/TimeSheetForm")
 public String ReturnTimeSheet(Model model){
@@ -26,6 +42,38 @@ public String ReturnTimeSheet(Model model){
             return "contractor_timesheet";
         }
 
+        return "timesheet_confirmation";
+    }
+
+    @RequestMapping(path = "/TimeSheetConfirm", method = RequestMethod.POST)
+    public String confirmTimeSheet(@ModelAttribute("TimeSheetKey") @Valid TimeSheetForm timeSheet,
+                                 BindingResult bindingResult,
+                                 Model model,
+                                 HttpSession session) {
+
+        if (bindingResult.hasErrors()) {
+            LOG.error(bindingResult.toString());
+            LOG.error("TimeSheet Form has binding errors");
+            return "contractor_timesheet";
+        }
+
+        TimeSheet t = new TimeSheet(
+                null,
+                null,
+                timeSheet.getMonday_worked(),
+                timeSheet.getTuesday_worked(),
+                timeSheet.getWednesday_worked(),
+                timeSheet.getThursday_worked(),
+                timeSheet.getFriday_worked(),
+                timeSheet.getSaturday_worked(),
+                timeSheet.getSunday_worked(),
+                timeSheet.getOvertime(),
+                timeSheet.getStart_date(),
+                "Pending");
+
+
+
+        TimeSheetCreator.save(t);
         return "timesheet_confirmation";
     }
 }
