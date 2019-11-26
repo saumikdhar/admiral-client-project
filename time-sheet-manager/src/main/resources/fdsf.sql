a77008a7a845f4b5f080a85f9f6db90df96d7607
Saumik Dhar <dhars@cardiff.ac.uk>
2019-11-26 21:47:38 +0000

    implement drop down feature to select agency

--- time-sheet-manager/src/main/resources/fdsf.sql
+++
@@ -1,8 +0,0 @@
-USE managingtimesheets;
-SELECT *
-FROM contractors c
-         JOIN agency_contractors ac ON c.contractor_id = ac.contractor_id
-         JOIN agencies a ON ac.agency_id = a.agency_id
-WHERE c.contractor_id = 7;
-
-SELECT * FROM agency_contractors where managingtimesheets.agency_contractors.contractor_id = 7;
