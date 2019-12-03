646b8174e592c08373748299f8f1e8f64503b5e8
c1825201 <rogersc2@cardiff.ac.uk>
2019-12-03 19:01:02 +0000

    edited timesheet sql

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/TimeSheetRepositoryJpa.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/TimeSheetRepositoryJpa.java
@@ -14,10 +14,8 @@ public interface TimeSheetRepositoryJpa extends JpaRepository<TimeSheet, Long>, 
     /*Queries go in here*/
 
 //  Gets all time sheets where status is pending and for the relevant manager
-    @Query(value = "select * from timesheets join " +
-        "agency_contractors ac on timesheets.agency_contractor_id = " +
-        "ac.agency_contractor_id join contractors c2 on ac.contractor_id = " +
-        "c2.contractor_id join managers m on c2.manager_id = m.manager_id where " +
+    @Query(value = "select * from timesheets t join contractors c on t.contractor_id = " +
+        "c.contractor_id join managers m on c.manager_id = m.manager_id where " +
         "m.manager_first_name = :firstName and m.manager_last_name = :lastName and status like '%pending%'", nativeQuery = true)
     public List<TimeSheet> getTimeSheetsByManager(@Param("lastName") String lastName,@Param("firstName") String firstName);
 
