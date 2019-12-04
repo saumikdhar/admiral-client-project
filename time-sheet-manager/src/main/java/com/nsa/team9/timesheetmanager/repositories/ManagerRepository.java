ca011ed8ceb83875135bc9d1615090a2f38ecbde
Gabriel Agius <AgiusG@cardiff.ac.uk>
2019-12-04 11:54:29 +0000

    adding files

---
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/repositories/ManagerRepository.java
@@ -0,0 +1,11 @@
+package com.nsa.team9.timesheetmanager.repositories;
+
+import com.nsa.team9.timesheetmanager.domain.Manager;
+
+import java.util.List;
+
+public interface ManagerRepository {
+
+    /*finds all managers*/
+    List<Manager> findAllManagers();
+}
