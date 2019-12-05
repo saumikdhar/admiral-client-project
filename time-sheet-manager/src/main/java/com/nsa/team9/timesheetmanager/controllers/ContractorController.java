113e1626d2b1fd77388e995d221e2bcc4542d76a
Saumik Dhar <dhars@cardiff.ac.uk>
2019-12-05 22:07:36 +0000

    resolve merge

--- time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ContractorController.java
+++ time-sheet-manager/src/main/java/com/nsa/team9/timesheetmanager/controllers/ContractorController.java
@@ -2,41 +2,24 @@ package com.nsa.team9.timesheetmanager.controllers;
 
 import com.nsa.team9.timesheetmanager.config.security.MyUserPrincipal;
 import com.nsa.team9.timesheetmanager.domain.*;
-<<<<<<< HEAD
-import com.nsa.team9.timesheetmanager.services.AgencyContractorSearchImpl;
 import com.nsa.team9.timesheetmanager.services.AgencySearchImpl;
 import com.nsa.team9.timesheetmanager.services.TimeSheetSearch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-=======
 import com.nsa.team9.timesheetmanager.services.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
->>>>>>> develop
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-=======
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
->>>>>>> develop
-
-import javax.mail.MessagingException;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-<<<<<<< HEAD
-import java.io.IOException;
-=======
->>>>>>> develop
-import java.util.List;
 
 @Service
 @SessionAttributes({"agencies"})
@@ -66,24 +49,12 @@ public class ContractorController {
         return "contractor_timesheet";
     }
 
-<<<<<<< HEAD
     @Value("${gmail.username}")
     private String username;
 
     @Value("${gmail.password}")
     private String password;
 
-    @PostMapping("TimeSheetDetails")
-    public String TimeSheetDetails(Model model,
-                                   @ModelAttribute("agencycontractor") AgencyContractorForm agencyContractorForm,
-                                   @ModelAttribute("TimeSheet") @Valid TimeSheetForm TimeSheet,
-                                   BindingResult bindingResult) throws IOException, MessagingException {
-
-
-        Login l = new Login(2L,"quis.arcu.vel@augueporttitor.org","Quisque","1");
-        Manager m = new Manager(2L,"Felix","Shaffer",l);
-        Contractor c = new Contractor(7L,"gabriel","agius",l,m);
-=======
     @PostMapping("/TimeSheetDetails")
     public String TimeSheetDetails(Model model,
                                    @ModelAttribute("agencycontractor") AgencyContractorForm agencyContractorForm,
@@ -99,7 +70,6 @@ public class ContractorController {
 
 //        Login l = new Login(2L,"quis.arcu.vel@augueporttitor.org","Quisque",1);
 //        Manager m = new Manager(2L,"Felix","Shaffer",l);
->>>>>>> develop
         Agency a = agencyContractorForm.getAgency_id();
 
         if (bindingResult.hasErrors()) {
@@ -125,13 +95,10 @@ public class ContractorController {
         System.out.println("saved timesheet " + t.toString());
         TimeSheetCreator.createTimeSheet(t);
 
-<<<<<<< HEAD
-=======
         //invalidate session without removing login
         status.setComplete();
         session.removeAttribute("agencies");
 
->>>>>>> develop
         return "timesheet_confirmation";
 
         //Code here to get the post request running when page is returned
